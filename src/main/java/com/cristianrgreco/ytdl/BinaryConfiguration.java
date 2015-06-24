28f69864bd1bbc6a67716593082f84c1c022e8fa
Cristian Greco <cristian.greco@itv.com>
2015-06-24 10:58:06 +0100

    BinaryConfiguration processes cleanup.

--- src/main/java/com/cristianrgreco/ytdl/BinaryConfiguration.java
+++ src/main/java/com/cristianrgreco/ytdl/BinaryConfiguration.java
@@ -2,7 +2,6 @@ package com.cristianrgreco.ytdl;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,23 +23,21 @@ public class BinaryConfiguration implements BaseBinaryConfiguration {
     }
 
     private static boolean areBinariesFunctioningCorrectly(File youTubeDlBinary, File ffmpegBinary, File ffprobeBinary) {
-        List<String> youtubeDlCommand = new ArrayList<>(Arrays.asList(youTubeDlBinary.getAbsolutePath(), "--version"));
-        List<String> ffmpegCommand = new ArrayList<>(Arrays.asList(ffmpegBinary.getAbsolutePath(), "-version"));
-        List<String> ffProbeCommand = new ArrayList<>(Arrays.asList(ffprobeBinary.getAbsolutePath(), "-version"));
-
         try {
-            return new ArrayList<>(Arrays.asList(
-                    new ProcessBuilder(youtubeDlCommand).start(),
-                    new ProcessBuilder(ffmpegCommand).start(),
-                    new ProcessBuilder(ffProbeCommand).start())
-            ).parallelStream().map(process -> {
-                try {
-                    process.waitFor();
-                    return process.exitValue();
-                } catch (InterruptedException e) {
-                    return -1;
-                }
-            }).allMatch(status -> status == 0);
+            List<Process> processes = Arrays.asList(
+                    new ProcessBuilder(Arrays.asList(youTubeDlBinary.getAbsolutePath(), "--version")).start(),
+                    new ProcessBuilder(Arrays.asList(ffmpegBinary.getAbsolutePath(), "-version")).start(),
+                    new ProcessBuilder(Arrays.asList(ffprobeBinary.getAbsolutePath(), "-version")).start());
+            return processes.parallelStream()
+                    .map(process -> {
+                        try {
+                            process.waitFor();
+                            return process.exitValue();
+                        } catch (InterruptedException e) {
+                            return -1;
+                        }
+                    })
+                    .allMatch(status -> status == 0);
         } catch (IOException e) {
             return false;
         }
